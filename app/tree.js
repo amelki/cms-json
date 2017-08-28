652ebd15a146bc41e955d88ce468079e7cb4fba7
Antoine-Melki <Antoine.Melki@ibm.com>
2017-08-29 00:10:17 +0200

    Styling

--- app/tree.js
+++ app/tree.js
@@ -8,9 +8,11 @@ const Tree = ({model, selection}) => {
 	if (model.children && model.children.length > 0) {
 		for (let i = 0; i < model.children.length; i++) {
 			const child = model.children[i];
-			all.push(<Node key={i} node={child} path={''} selection={selection}/>);
+			all.push(<Node key={i} node={child} path={''} selection={selection} depth={1}/>);
 		}
-		return <nav>{all}</nav>;
+		return <nav>
+			<ul>{all}</ul>
+		</nav>;
 	}
 	return <span/>;
 };
@@ -22,30 +24,35 @@ Tree.propTypes = {
 	selection: PropTypes.object.isRequired
 };
 
-export const Node = ({node, path, selection}) => {
+export const Node = ({node, path, selection, depth}) => {
 	const newPath = path + "/" + Cms.slugify(node.name);
 	const linkClass = (('/' + selection.treePath) === newPath) ? 'selected' : '';
 	const prefix = "/node";
 	let subList = "";
 	if (node.children && node.children.length > 0) {
 		const children = [];
+		const newDepth = depth + 1;
 		for (let i = 0; i < node.children.length; i++) {
 			const child = node.children[i];
-			children.push(<Node key={i} node={child} path={newPath} selection={selection}/>);
+			children.push(<Node key={i} node={child} path={newPath} selection={selection} depth={newDepth}/>);
 		}
-		subList = <ul>{children}</ul>;
+		subList = children;
 	}
+	const space = (20 * depth) + 'px';
 	return (
-		<li>
-			<Link to={ prefix + newPath } className={linkClass}>{node.name}</Link>
+		<span>
+			<li>
+				<Link className={linkClass} style={{paddingLeft: space}} to={ prefix + newPath }>{node.name}</Link>
+			</li>
 			{subList}
-		</li>
+		</span>
 	);
 };
 
 Node.propTypes = {
 	node: PropTypes.object.isRequired,
 	path: PropTypes.string.isRequired,
-	selection: PropTypes.object.isRequired
+	selection: PropTypes.object.isRequired,
+	depth: PropTypes.number.isRequired
 };
 
